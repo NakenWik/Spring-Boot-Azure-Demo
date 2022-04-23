536ba29e38b19e182eee1124e8c2d2643d33c67b
Wikaha <Naken.Wikaha@Chubb.com>
2022-04-23 13:36:02 +0700

    first commit

---
+++ src/main/java/com/itsdm/azure/demo/SpringBootDemoApplication.java
@@ -0,0 +1,21 @@
+package com.itsdm.azure.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDemoApplication {
+	
+	@GetMapping("/message")
+	public String message() {
+		return "Hello World..";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+}
