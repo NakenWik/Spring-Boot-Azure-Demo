74ac55251384f72d3ac55c55090a9f69c5dc6d49
nxwika <nxwika@APTHLP-ELPT2009.aceins.com>
2022-04-24 09:38:33 +0700

    add welcome message

--- src/main/java/com/itsdm/azure/demo/SpringBootDemoApplication.java
+++ src/main/java/com/itsdm/azure/demo/SpringBootDemoApplication.java
@@ -3,13 +3,14 @@ package com.itsdm.azure.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringBootDemoApplication {
 
-	@GetMapping("/")
+	@RequestMapping
 	public String WelcomePage() {
 		System.out.println("Hi..");
 		return "Hello WellcomePage..";
